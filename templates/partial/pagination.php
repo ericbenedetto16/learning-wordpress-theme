7608730aaa79802d0f9c9be3f5e5d35a279d49a8
Eric Benedetto <ericsplace3@gmail.com>
2019-05-27 00:08:35 -0400

    added pagination template file

---
+++ templates/partial/pagination.php
@@ -0,0 +1,4 @@
+<div class="container-fluid row justify-content-center">
+    <div class="col-4 text-left"><?php previous_post_link(); ?></div>
+    <div class="col-4 text-right"><?php next_post_link(); ?></div>
+</div>
