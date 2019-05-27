9b5ba254c98f2253770f67b64ceed6aca8c5984a
Eric Benedetto <ericsplace3@gmail.com>
2019-05-27 00:09:40 -0400

    restructured post pages. added main wrapper for styling

--- templates/partial/post-image.php
+++ templates/partial/post-image.php
@@ -1,9 +1,12 @@
-<article <?php post_class(); ?>>
-    <a href="<?php the_permalink(); ?>"><h2><?php the_title(); ?></h2></a>
-    <div class="meta-info">
-        <p>Posted by <?php the_author_posts_link(); ?></p>
-        <p>Categories <?php the_category( ' ' ); ?></p>
-        <p><?php the_tags( 'Tags: ', ', ' ); ?></p>
-    </div>
-    <a href="<?php the_permalink(); ?>"><?php the_content(); ?></a>
-</article>
+<main class="article" id="post-<?php the_ID(); ?>">
+    <article class="mt-5">
+        <h2><?php the_title(); ?></h2>
+        <div class="meta-info">
+            <p>Posted by <?php the_author_posts_link(); ?></p>
+            <p>Categories <?php the_category( ', ' ); ?></p>
+            <p><?php the_tags( 'Tags: ', ', ' ); ?></p>
+        </div>
+        <?php the_content(); ?>
+        <?php get_template_part( 'templates/partial/comments' ) ?>
+    </article>
+</main>
--- templates/partial/post-video.php
+++ templates/partial/post-video.php
@@ -1,9 +1,12 @@
-<article <?php post_class(); ?>>
-    <a href="<?php the_permalink(); ?>"><h2><?php the_title(); ?></h2></a>
-    <div class="meta-info">
-        <p>Posted by <?php the_author_posts_link(); ?></p>
-        <p>Categories <?php the_category( ' ' ); ?></p>
-        <p><?php the_tags( 'Tags: ', ', ' ); ?></p>
-    </div>
-    <p><?php the_content(); ?></p>
-</article>
+<main class="article" id="post-<?php the_ID(); ?>">
+    <article class="mt-5">
+        <h2><?php the_title(); ?></h2>
+        <div class="meta-info">
+            <p>Posted by <?php the_author_posts_link(); ?></p>
+            <p>Categories <?php the_category( ', ' ); ?></p>
+            <p><?php the_tags( 'Tags: ', ', ' ); ?></p>
+        </div>
+        <p><?php the_content(); ?></p>
+        <?php get_template_part( 'templates/partial/comments' ) ?>
+    </article>
+</main>
