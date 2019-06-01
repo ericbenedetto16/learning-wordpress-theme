cbcc2e1e2dd5c216f4f71f913ade7ab973c699ad
Eric Benedetto <ericsplace3@gmail.com>
2019-06-01 19:48:17 -0400

    fixed iframe attributes for validation

--- sidebar-right.php
+++ sidebar-right.php
@@ -11,10 +11,10 @@
     <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_blank">
     <input type="hidden" name="cmd" value="_s-xclick">
     <input type="hidden" name="hosted_button_id" value="7HFWZJMBKC34N">
-    <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
-    <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+    <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" name="submit" alt="PayPal - The safer, easier way to pay online!">
+    <img alt="paypal button" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
     </form>
-  </div>
+  </section>
   <section class="pay-dues">
     <?php
       // if( is_active_sidebar( 'sidebar-2' ) ) {
