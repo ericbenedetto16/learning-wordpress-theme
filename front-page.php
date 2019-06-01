9df2b47117642cbd47e81bbc692c41ab0ee6b407
Eric Benedetto <ericsplace3@gmail.com>
2019-06-01 19:45:43 -0400

    added alt tags to all img

--- front-page.php
+++ front-page.php
@@ -25,22 +25,22 @@
     </div>
     <section class="banner-container">
       <section class="social-holder">
-        <a class="social-badge" href="<?php echo get_theme_mod( 'set_badgeOne_link' ); ?>" target=_blank><img class="social-badge" src="<?php echo get_theme_mod( 'set_badgeOne_img' ); ?>" ></a>
-        <a class="social-badge" href="<?php echo get_theme_mod( 'set_badgeTwo_link' ); ?>" target=_blank><img class="social-badge" src="<?php echo get_theme_mod( 'set_badgeTwo_img' ); ?>"></a>
+        <a class="social-badge" href="<?php echo get_theme_mod( 'set_badgeOne_link' ); ?>" target=_blank><img class="social-badge" src="<?php echo get_theme_mod( 'set_badgeOne_img' ); ?>" alt="social badge" ></a>
+        <a class="social-badge" href="<?php echo get_theme_mod( 'set_badgeTwo_link' ); ?>" target=_blank><img class="social-badge" src="<?php echo get_theme_mod( 'set_badgeTwo_img' ); ?>" alt="social badge"></a>
       </section>
       <p class="banner-text"><?php echo get_theme_mod( 'set_socialBanner_text' ); ?></p>
       <section class="social-holder">
-        <a class="social-badge" href="<?php echo get_theme_mod( 'set_badgeThree_link' ); ?>" target=_blank><img class="social-badge" src="<?php echo get_theme_mod( 'set_badgeThree_img' ); ?>"></a>
-        <a class="social-badge" href="<?php echo get_theme_mod( 'set_badgeFour_link' ); ?>" target=_blank><img class="social-badge" src="<?php echo get_theme_mod( 'set_badgeFour_img' ); ?>"></a>
+        <a class="social-badge" href="<?php echo get_theme_mod( 'set_badgeThree_link' ); ?>" target=_blank><img class="social-badge" src="<?php echo get_theme_mod( 'set_badgeThree_img' ); ?>" alt="social badge"></a>
+        <a class="social-badge" href="<?php echo get_theme_mod( 'set_badgeFour_link' ); ?>" target=_blank><img class="social-badge" src="<?php echo get_theme_mod( 'set_badgeFour_img' ); ?>" alt="social badge"></a>
       </section>
     </section>
     <section class="widget-container">
-      <iframe class="googlecalendar" src="<?php echo get_theme_mod( 'set_calendar_url' ); ?>" frameborder="0">
+      <iframe class="googlecalendar" src="<?php echo get_theme_mod( 'set_calendar_url' ); ?>" style="border:0">
       </iframe>
 
       <div class="twitter-frame">
         <a class="twitter-timeline" href="<?php echo get_theme_mod( 'set_twitterUrl' ); ?>" data-height="500"></a>
-        <script async src="https://platform.twitter.com/widgets.js"charset="utf-8"></script>
+        <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
       </div>
     </section>
   </main>
