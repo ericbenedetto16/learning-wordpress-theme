176acd1176150a225ec7de0e0c2b9756dad076ba
Eric Benedetto <ericsplace3@gmail.com>
2019-06-01 22:30:33 -0400

    structural change for elements in proper parent container

--- single.php
+++ single.php
@@ -1,10 +1,8 @@
 <?php 
 get_header(); 
     while( have_posts() ):
-        get_template_part( 'templates/partial/pagination' );
         the_post();
         get_template_part( 'templates/partial/post', get_post_format() );
     endwhile;
-    
 get_footer(); 
 ?>
