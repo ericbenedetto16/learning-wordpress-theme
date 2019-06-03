c90ffa48f4af629ed0fdd918fb387114caacd5df
Eric Benedetto <ericsplace3@gmail.com>
2019-06-02 23:12:06 -0400

    restructuring and bug fixing

--- templates/archive.php
+++ templates/archive.php
@@ -1,43 +1,30 @@
 <?php get_header(); ?>
     <div class="content-area">
-        <main>
+        <main class="article">
             <section class="middle-area">
-                <div class="container-fluid">
-                    <div class="row">
-                        <div class="archive col-md-9">
-                            <?php
+                <div class="archive">
+                    <?php
 
-                                the_archive_title( '<h1 class="archive-title">', '</h1>' );
-                                the_archive_description();
+                        the_archive_title( '<h1 class="archive-title">', '</h1>' );
+                        the_archive_description();
 
-                                //If there are posts
-                                if( have_posts() ):
-                                    //While Posts, show the posts
-                                    while( have_posts() ): the_post();
-                                    
-                                    get_template_part( 'templates/partial/post', get_post_format() );
-                                    
-                                    endwhile;
-                                 
-                                    // get_template_part( 'templates/partial/pagination' );
-
-                                else:
-                            ?>
-                                    <script>console.log('no posts');</script>
-                                    
-                                    <p><?php _e( "There's nothing yet to be displayed!", 'schoolroot' ); ?></p>
-                            <?php 
-                                 endif; 
-                            ?>
-                        </div>
-                        <aside class="sidebar col-md-3 h-100">
-                        </aside>
-                    </div>
-                </div>
-            </section>
-            <section class="map">
-                <div class="container-fluid">
-                    <div class="row">Map</div>
+                        //If there are posts
+                        if( have_posts() ):
+                            //While Posts, show the posts
+                            while( have_posts() ): the_post();
+                            
+                            get_template_part( 'templates/partial/archive-preview' );
+                            
+                            endwhile;
+                            
+                        else:
+                    ?>
+                            <script>console.log('no posts');</script>
+                            
+                            <p><?php _e( "There's nothing yet to be displayed!", 'schoolroot' ); ?></p>
+                    <?php 
+                            endif; 
+                    ?>
                 </div>
             </section>
         </main>
